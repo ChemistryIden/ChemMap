dedb00c9a1fe8bc877823c60ad503da35e61a424
Mervyn <youngfields@gmail.com>
2026-08-08 20:00:08 +0800

    Update August inspection points

--- august-inspection-map/map_data.js
+++ august-inspection-map/map_data.js
@@ -1,16 +1,14 @@
 window.AUGUST_INSPECTION_MAP_DATA = {
-  generatedAt: "2026-08-02",
+  generatedAt: "2026-08-08",
   coordinateSource: "天地图地理编码",
   points: [
-    {id:1,company:"上海艺杏食品有限公司",address:"江桥镇宝园三路617号",coordinates:[121.26345,31.28713],geocodeLevel:"门址",geocodeScore:100},
-    {id:2,company:"上海赐合展览设计制作有限公司",address:"南翔镇嘉美路1788号",coordinates:[121.256821,31.298202],geocodeLevel:"门址",geocodeScore:100},
-    {id:3,company:"上海贺寿食品有限公司",address:"江桥镇宝园七路469号",coordinates:[121.254286,31.285527],geocodeLevel:"门址",geocodeScore:100},
-    {id:4,company:"上海欧风展览服务有限公司",address:"江桥镇宝园一路588号",coordinates:[121.266783,31.286007],geocodeLevel:"门址",geocodeScore:100},
-    {id:5,company:"上海帑泉金属制品公司（上海造币有限公司封浜厂区）",address:"江桥镇曹安公路3451号",coordinates:[121.290707,31.26917],geocodeLevel:"门址",geocodeScore:100},
-    {id:6,company:"上海百适乐运动科技有限公司（原名：上海荣威塑胶工业有限公司）",address:"江桥镇金园五路208号",coordinates:[121.303698,31.262539],geocodeLevel:"企业兴趣点（按原企业名检索）",geocodeScore:66},
-    {id:7,company:"上海辛格林纳新时达电机有限公司",address:"南翔镇新勤路289号",coordinates:[121.29605,31.32122],geocodeLevel:"门址",geocodeScore:100},
-    {id:8,company:"上海电信实业集团有限公司云杉苑分公司",address:"嘉定区沪宜公路1175号",coordinates:[121.2959,31.29823],geocodeLevel:"门址",geocodeScore:100},
-    {id:9,company:"上海典鸿智能科技有限公司",address:"嘉定区南翔镇嘉美路1988号5号",coordinates:[121.25157,31.2997],geocodeLevel:"门址",geocodeScore:100},
-    {id:10,company:"上海盈固化工有限公司",address:"嘉定区金沙江西路1555弄392号",coordinates:[121.32455,31.2424],geocodeLevel:"门址",geocodeScore:100}
+    {id:1,company:"上海艺杏食品有限公司",shortName:"艺杏食品",address:"江桥镇宝园三路617号",coordinates:[121.26345,31.28713],geocodeLevel:"门址",geocodeScore:100},
+    {id:2,company:"上海贺寿食品有限公司",shortName:"贺寿食品",address:"江桥镇宝园七路469号",coordinates:[121.254286,31.285527],geocodeLevel:"门址",geocodeScore:100},
+    {id:3,company:"上海欧风展览服务有限公司",shortName:"欧风展览",address:"江桥镇宝园一路588号",coordinates:[121.266783,31.286007],geocodeLevel:"门址",geocodeScore:100},
+    {id:4,company:"上海帑泉金属制品公司（上海造币有限公司封浜厂区）",shortName:"帑泉金属",address:"江桥镇曹安公路3451号",coordinates:[121.290707,31.26917],geocodeLevel:"门址",geocodeScore:100},
+    {id:5,company:"上海百适乐运动科技有限公司（原名：上海荣威塑胶工业有限公司）",shortName:"百适乐运动",address:"江桥镇金园五路208号",coordinates:[121.303698,31.262539],geocodeLevel:"企业兴趣点（按原企业名检索）",geocodeScore:66},
+    {id:6,company:"上海辛格林纳新时达电机有限公司",shortName:"新时达电机",address:"南翔镇新勤路289号",coordinates:[121.29605,31.32122],geocodeLevel:"门址",geocodeScore:100},
+    {id:7,company:"上海昕悦实业有限公司",shortName:"昕悦实业",address:"江桥镇宝园五路399号",coordinates:[121.257944,31.284322],geocodeLevel:"门址",geocodeScore:100},
+    {id:8,company:"考拉爱车",shortName:"考拉爱车",address:"江桥镇曹安公路3800号",coordinates:[121.27673,31.27277],geocodeLevel:"门址",geocodeScore:100}
   ]
 };
